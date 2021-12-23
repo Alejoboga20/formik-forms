9db1ff843168975e4a28dad2f414af2e71b219b1
Alejoboga20 <alejoboga20@gmail.com>
2021-12-23 16:15:50 -0300

    Adding visual validations

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -13,10 +13,21 @@ export const useForm = <T extends Object>(initialState: T) => {
 		console.log(formData);
 	};
 
+	const resetForm = () => setFormData({ ...initialState });
+
+	const isValidEmail = (email: string) => {
+		const re =
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return re.test(email);
+	};
+
 	return {
 		...formData,
 		formData,
+
+		isValidEmail,
 		onChange,
 		onSubmit,
+		resetForm,
 	};
 };
--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -9,14 +9,41 @@ const initialFormValues = {
 };
 
 export const RegisterPage = () => {
-	const { name, email, password1, password2, onChange, onSubmit } = useForm(initialFormValues);
+	const { name, email, password1, password2, isValidEmail, onChange, onSubmit, resetForm } =
+		useForm(initialFormValues);
 
 	return (
 		<div>
 			<h1>Register Page</h1>
 			<form onSubmit={onSubmit}>
-				<input type='text' placeholder='Name' value={name} onChange={onChange} name='name' />
-				<input type='email' placeholder='Email' value={email} onChange={onChange} name='email' />
+				<input
+					type='text'
+					placeholder='Name'
+					value={name}
+					onChange={onChange}
+					name='name'
+					className={`${name.trim().length <= 0 && 'has-error'} `}
+				/>
+				{name.trim().length <= 0 && (
+					<span>
+						<small>Field Required</small>
+					</span>
+				)}
+
+				<input
+					type='email'
+					placeholder='Email'
+					value={email}
+					onChange={onChange}
+					name='email'
+					className={`${!isValidEmail(email) && 'has-error'} `}
+				/>
+				{!isValidEmail(email) && (
+					<span>
+						<small>Invalid Email</small>
+					</span>
+				)}
+
 				<input
 					type='password'
 					placeholder='Password'
@@ -24,6 +51,17 @@ export const RegisterPage = () => {
 					onChange={onChange}
 					name='password1'
 				/>
+				{password1.trim().length <= 0 && (
+					<span>
+						<small>Field Required</small>
+					</span>
+				)}
+				{password1.trim().length < 6 && (
+					<span>
+						<small>Password length should be 6</small>
+					</span>
+				)}
+
 				<input
 					type='password'
 					placeholder='Repeat Password'
@@ -31,8 +69,21 @@ export const RegisterPage = () => {
 					onChange={onChange}
 					name='password2'
 				/>
+				{password1.trim().length <= 0 && (
+					<span>
+						<small>Field Required</small>
+					</span>
+				)}
+				{password1.trim() !== password2.trim() && (
+					<span>
+						<small>Passwords don't match</small>
+					</span>
+				)}
 
 				<button type='submit'>Create</button>
+				<button type='button' onClick={resetForm}>
+					Reset Form
+				</button>
 			</form>
 		</div>
 	);
