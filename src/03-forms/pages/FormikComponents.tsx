b1ad1442435ade3675810a941302f0f72cd8d84a
Alejoboga20 <alejoboga20@gmail.com>
2021-12-27 10:12:45 -0300

    Using checkboxes and select

--- src/03-forms/pages/FormikComponents.tsx
+++ src/03-forms/pages/FormikComponents.tsx
@@ -8,7 +8,7 @@ export const FormikComponents = () => {
 			<h1>Formik Components Tutorial</h1>
 
 			<Formik
-				initialValues={{ firstName: '', lastName: '', email: '' }}
+				initialValues={{ firstName: '', lastName: '', email: '', terms: false, jobType: '' }}
 				onSubmit={(values) => {
 					console.log(values);
 				}}
@@ -16,6 +16,10 @@ export const FormikComponents = () => {
 					firstName: Yup.string().max(15, 'Should have 15 chars or less').required('Required'),
 					lastName: Yup.string().max(10, 'Should have 10 chars or less').required('Required'),
 					email: Yup.string().email('Should be valid email').required('Required'),
+					jobType: Yup.string()
+						.oneOf(['developer', 'designer', 'tester'], 'Option not allowed')
+						.required('Required'),
+					terms: Yup.boolean().oneOf([true], 'Terms should be accepted'),
 				})}
 			>
 				{(formik) => (
@@ -29,8 +33,25 @@ export const FormikComponents = () => {
 						<ErrorMessage name='lastName' component='span' />
 
 						<label htmlFor='email'>Email</label>
-						<Field name='email' type='email' />
+						<Field name='email' type='text' />
 						<ErrorMessage name='email' component='span' />
+
+						<label htmlFor='jobType'>Job Type</label>
+						<Field name='jobType' as='select'>
+							<option value=''>Pick something</option>
+							<option value='developer'>Developer</option>
+							<option value='designer'>Designer</option>
+							<option value='tester'>Tester</option>
+							<option value='it'>It-Jr</option>
+						</Field>
+						<ErrorMessage name='jobType' component='span' />
+
+						<label>
+							<Field name='terms' type='checkbox' />
+							Terms and Conditions
+						</label>
+
+						<ErrorMessage name='terms' component='span' />
 
 						<button type='submit'>Submit</button>
 					</Form>
